75abd1f5613b7557b6264c0b469db473767206fd
Nelson Aparecido Fornazeiro <nelson.fornazeiro@avanade.com>
2026-03-30 13:55:23 -0300

    chore: criação do projeto

---
+++ src/main/java/com/agro/sensores/AgroSensoresApplication.java
@@ -0,0 +1,13 @@
+package com.agro.sensores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSensoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroSensoresApplication.class, args);
+	}
+
+}
